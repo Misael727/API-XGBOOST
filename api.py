6b07450185d8bd15e4017bf50bff1e9b3908d6d2
Misael Moreira <116930174+Misael727@users.noreply.github.com>
2025-07-09 13:10:39 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import xgboost as xgb
 import pandas as pd
 from typing import List
 
-# Carrega o modelo treinado (model.json deve estar em IA/)
+# Carrega o modelo treinado
 model = xgb.Booster()
 model.load_model("model.bin")
 
 app = FastAPI(title="API de Predição XGBoost")
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://www.synapseia.tech", "https://synapseia.tech"],  # ou ["*"] em dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Payload(BaseModel):
     idade: int
